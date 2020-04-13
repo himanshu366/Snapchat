b38fc3a664fd42d9b9517c3d1e8b9074880e2394
Himanshu <himanshuguptax1@gmail.com>
2020-04-13 10:38:56 +0530

    Initial

--- Snapchat/app/src/main/java/com/example/snapchat/CreateSnapActivity.kt
+++ Snapchat/app/src/main/java/com/example/snapchat/CreateSnapActivity.kt
@@ -1,9 +1,15 @@
  package com.example.snapchat
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.ImageView
 import androidx.core.widget.ImageViewCompat
+//import java.util.jar.Manifest
+ import android.Manifest
 
  class CreateSnapActivity : AppCompatActivity() {
 
@@ -13,4 +19,19 @@ import androidx.core.widget.ImageViewCompat
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snap)
     }
+
+     fun getPhoto() {
+         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+         startActivityForResult(intent, 1)
+     }
+
+     fun chooseImageClicked(view: View) {
+         if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+             requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
+     } else {
+         getPhoto()
+         }
+     }
+
 }
